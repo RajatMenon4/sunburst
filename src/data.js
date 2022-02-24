4716cabd0f5677a86583d01e95e84c60d8ca11cb
Rajat Menon <Rajat.Menon@aqura.com.au>
2022-02-24 11:19:26 +0800

    Updated

---
+++ src/data.js
@@ -0,0 +1,91 @@
+export default {
+  "name": "Data",
+      "children": [
+        {
+          "name": "Jeff",
+          "children": [
+            {
+              "name": "Devices",
+              "children": [
+                {
+                  "name": "afer", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "svs", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "sdfv", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "svdvs", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "svfv", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "svsdfv", "minSize": 10, "size": 3000
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Name 2",
+          "children": [
+            {
+              "name": "Devices",
+              "children": [
+                {
+                  "name": "svs", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "sdvsdv", "minSize": 10, "size": 3000
+                },
+              ]
+            },
+          ]
+        },
+        {
+        "name": "Name F",
+        "children": [
+          {
+            "name": "Devices",
+            "children": [
+              {
+                "name": "dcsadvvd", "minSize": 10, "size": 3000
+              },
+            ]
+          }
+        ]
+        },
+        {
+          "name": "Rajat Menon",
+          "children": [
+            {
+              "name": "Devices",
+              "children": [
+                {
+                  "name": "dascvdvc", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "sadvcdsv","minSize": 10, "size": 3000
+                },
+              ]
+            },
+          ]
+        },
+        {
+          "name": "Ronaldo",
+          "children": [
+            {
+              "name": "Devices",
+              "children": [
+                {
+                  "name": "acadca", "minSize": 10, "size": 3000
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
